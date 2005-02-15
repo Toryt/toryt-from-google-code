ebfc96a919c7e67afc72d727f3b5f26637af2b4b
rclerckx <rclerckx@021d918c-914f-0410-bf23-5b6681338975>
2005-02-15 14:50:20 +0000

    Made _Contract_Bookmark compilable.

--- trunk/toryt/src/example/org/toryt/example/_Contract_Bookmark.java
+++ trunk/toryt/src/example/org/toryt/example/_Contract_Bookmark.java
@@ -5,6 +5,8 @@ import org.toryt.TorytException;
 import org.toryt.hard.ClassContract;
 import org.toryt.hard.ConstructorContract;
 import org.toryt.hard.MutatorContract;
+import org.toryt.support.straightlist.StraightList;
+import org.toryt.support.straightlist.LazyMappingStraightList.Mapping;
 
 
 public class _Contract_Bookmark extends ClassContract {
@@ -12,25 +14,61 @@ public class _Contract_Bookmark extends ClassContract {
   public _Contract_Bookmark() throws TorytException {
     super(Bookmark.class);
     setSuperClassContract("org.toryt.example.Node");
-    addClassMethodContract(new ConstructorContract(his, Bookmark.class, "Bookmark()") {
+    addConstructorContract(new ConstructorContract(this, Bookmark.class, "Bookmark()") {
+
+      public StraightList getTestCases() throws TorytException {
+        // TODO Auto-generated method stub
+        return null;
+      }
       
-                                });
-    addClassMethodContract(new ConstructorContract(this, Bookmark.class, "Bookmark(String, String, String, int, Group)") {
+    });
+
+    addConstructorContract(new ConstructorContract(this, Bookmark.class, "Bookmark(String, String, String, int, Group)") {
+
+      public StraightList getTestCases() throws TorytException {
+        // TODO Auto-generated method stub
+        return null;
+      }
       
                                 });
     addInstanceMethodContract(new MutatorContract(this, Bookmark.class, "setUrl(String)") {
+
+      public StraightList getTestCases() throws TorytException {
+        // TODO Auto-generated method stub
+        return null;
+      }
       
                                   });
     addInstanceMethodContract(new MutatorContract(this, Bookmark.class, "clicked()") {
+
+      public StraightList getTestCases() throws TorytException {
+        // TODO Auto-generated method stub
+        return null;
+      }
       
                                   });
     addInstanceMethodContract(new MutatorContract(this, Bookmark.class, "setRating(int)") {
+
+      public StraightList getTestCases() throws TorytException {
+        // TODO Auto-generated method stub
+        return null;
+      }
       
                                   });
     addBasicInspector("getUrl()");
     addBasicInspector("getNumberOfClicks()");
     addBasicInspector("getDateAdded()");
     close();
+  }
+
+  public StraightList getCasesMaps() throws TorytException {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public Mapping getCaseMapping() {
+    // TODO Auto-generated method stub
+    return null;
   }
   
 }
