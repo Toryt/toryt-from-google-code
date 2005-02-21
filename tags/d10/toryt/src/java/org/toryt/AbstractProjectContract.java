4ae52c0d35592f5e1d1a1841eeac51bd25af63c4
nobody <nobody@021d918c-914f-0410-bf23-5b6681338975>
2005-02-21 13:03:03 +0000

    This commit was manufactured by cvs2svn to create tag 'd10'.

---
+++ tags/d10/toryt/src/java/org/toryt/AbstractProjectContract.java
@@ -0,0 +1,58 @@
+package org.toryt;
+
+
+import java.util.Iterator;
+import java.util.Set;
+
+import org.toryt.support.straightlist.ConcatStraightList;
+import org.toryt.support.straightlist.StraightList;
+
+
+
+/**
+ * @author Jan Dockx
+ */
+public abstract class AbstractProjectContract
+    extends AbstractContract
+    implements ProjectContract {
+
+  /*<section name="Meta Information">*/
+  //  ------------------------------------------------------------------
+  /** {@value} */
+  public static final String CVS_REVISION = "$Revision$";
+  /** {@value} */
+  public static final String CVS_DATE = "$Date$";
+  /** {@value} */
+  public static final String CVS_STATE = "$State$";
+  /** {@value} */
+  public static final String CVS_TAG = "$Name$";
+  /*</section>*/
+
+  protected AbstractProjectContract(String projectName) {
+    assert projectName != null;
+    $projectName = projectName;
+  }
+  
+  public final String getProjectName() {
+    return $projectName;
+  }
+  
+  private String $projectName;
+  
+  public final StraightList getMethodTests() throws TorytException {
+    StraightList[] lists = new StraightList[getPackageContracts().size()];
+    Iterator iter = getPackageContracts().iterator();
+    int i = 0;
+    while (iter.hasNext()) {
+      PackageContract pc = (PackageContract)iter.next();
+      lists[i] = pc.getMethodTests();
+      i++;
+    }
+    return new ConcatStraightList(lists);
+  }
+  
+  public final Set getSubContracts() {
+    return getPackageContracts();
+  }
+
+}
