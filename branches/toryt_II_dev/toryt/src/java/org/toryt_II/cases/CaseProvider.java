168da8344c7bf61f6bb989d499a735fb4e73d5d4
jand <jand@021d918c-914f-0410-bf23-5b6681338975>
2006-04-27 17:52:30 +0000

    package is deprecated for now

---
+++ branches/toryt_II_dev/toryt/src/java/org/toryt_II/cases/CaseProvider.java
@@ -0,0 +1,61 @@
+package org.toryt_II.cases;
+
+
+import org.toryt.support.straightlist.NullFirstStraightList;
+import org.toryt.support.straightlist.StraightList;
+import org.toryt_II.TorytException;
+
+
+/**
+ * Implementations provide sets of test cases.
+ *
+ * @author    Jan Dockx
+ *
+ * @deprecated
+ */
+public interface CaseProvider {
+
+  /*<section name="Meta Information">*/
+  //------------------------------------------------------------------
+
+  /** {@value} */
+  public static final String CVS_REVISION = "$Revision$";
+  /** {@value} */
+  public static final String CVS_DATE = "$Date$";
+  /** {@value} */
+  public static final String CVS_STATE = "$State$";
+  /** {@value} */
+  public static final String CVS_TAG = "$Name$";
+
+  /*</section>*/
+
+
+  /**
+   * All possible relevant cases for this type,
+   * with <code>null</code>.
+   * @throws TorytException
+   */
+  NullFirstStraightList getCasesWithNull() throws TorytException;
+
+  /**
+   * All possible relevant cases for this type,
+   * without <code>null</code>.
+   * @throws TorytException
+   */
+  StraightList getCases() throws TorytException;
+
+  /**
+   * A limited number of most important cases for this type,
+   * without <code>null</code>.
+   * @throws TorytException
+   */
+  NullFirstStraightList getSomeCasesWithNull() throws TorytException;
+
+  /**
+   * A limited number of most important cases for this type,
+   * with <code>null</code>.
+   * @throws TorytException
+   */
+  StraightList getSomeCases() throws TorytException;
+
+}
