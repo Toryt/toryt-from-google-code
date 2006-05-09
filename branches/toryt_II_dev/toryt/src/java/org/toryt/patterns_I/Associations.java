68fbbb37422bdb5775a4f7e0ae9e19b31613bb07
jand <jand@021d918c-914f-0410-bf23-5b6681338975>
2006-05-09 22:04:56 +0000

    Fived

--- branches/toryt_II_dev/toryt/src/java/org/toryt/patterns_I/Associations.java
+++ branches/toryt_II_dev/toryt/src/java/org/toryt/patterns_I/Associations.java
@@ -1,13 +1,11 @@
 package org.toryt.patterns_I;
 
 
-import java.beans.IntrospectionException;
-import java.lang.reflect.InvocationTargetException;
-import java.util.Collection;
-import java.util.Iterator;
 import java.util.Set;
 
 import org.toryt.util_I.annotations.vcs.CvsInfo;
+import org.toryt.util_I.reflect.CannotGetPropertyException;
+import org.toryt.util_I.reflect.CannotGetValueException;
 import org.toryt.util_I.reflect.Reflection;
 
 
@@ -15,8 +13,6 @@ import org.toryt.util_I.reflect.Reflection;
  * Often used contracts for unidirectional and bidirectional associations.
  *
  * @author    Jan Dockx
- *
- * @toryt.import cC Collections;
  */
 @CvsInfo(revision = "$Revision$",
          date     = "$Date$",
@@ -31,7 +27,13 @@ public class Associations {
   /**
    * This method gathers the type invariants for a reference that implements
    * the to-one part of a bidirectional one-to-many association. The reference
-   * is optional or mandatory (weak type).
+   * is optional or mandatory (weak type). For a bidirectional associaton
+   * {@code A <-*as---b1-> B}, this method should be used as a type invariant
+   * in class {@code A}. For a mandatory association, e.g., the invariant should
+   * be {@code @invar bidrNTo1(getB(), "as", this, true);}. The invariant then
+   * expresses that, for all instances {@code a} of {@code A}, the instance
+   * {@code a.getB()} refers to has a {@link Set getAs()} that contains the instance
+   * {@code a}.
    *
    * @param     reference
    *            The object that <code>manyObject</code> refers to, that is on
@@ -56,113 +58,83 @@ public class Associations {
    * @return    (mandatory ==> (reference != null))
    *            && ((reference != null) ==>
    *                    reference[toManySetPropertyName].contains(manyObject));
+   *
+   * @todo use graphic representation here
    */
-  public static final boolean bidirNTo1(final Object reference,
-                                        final String toManySetPropertyName,
-                                        final Object manyObject,
-                                        final boolean mandatory) {
+  public static final <_Many_, _One_> boolean bidirNTo1(final _One_ reference,
+                                                        final String toManySetPropertyName,
+                                                        final _Many_ manyObject,
+                                                        final boolean mandatory) {
     assert manyObject != null;
-    boolean result;
+    boolean result = false;
     if (reference == null) {
       result = !mandatory;
     }
     else { // reference != null
       try {
         assert Reflection.hasPropertyReadMethod(reference.getClass(),
-                                           toManySetPropertyName);
+                                                toManySetPropertyName);
       }
-      catch (IntrospectionException iExc) {
-        assert false : "IntrospectionExceptionshould not happen: " + iExc; //$NON-NLS-1$
+      catch (CannotGetPropertyException e) {
+        assert false : "CannotGetPropertyException should not happen: " + e;
       }
-      Set manySet = null;
       try {
-        manySet = (Set)Reflection.getPropertyValue(reference, toManySetPropertyName);
+        @SuppressWarnings("unchecked") Set<_Many_> manySet =
+            (Set<_Many_>)Reflection.getPropertyValue(reference, toManySetPropertyName);
+        result = manySet.contains(manyObject);
       }
-      catch (NullPointerException npExc) {
-        assert false : "NullPointerExceptionshould not happen: " + npExc; //$NON-NLS-1$
+      catch (CannotGetValueException e) {
+        assert false : "CannotGetValueException should not happen: " + e;
       }
-      catch (IntrospectionException iExc) {
-        assert false : "IntrospectionExceptionshould not happen: " + iExc; //$NON-NLS-1$
-      }
-      catch (NoSuchMethodException nsmExc) {
-        assert false : "NoSuchMethodExceptionshould not happen: " + nsmExc; //$NON-NLS-1$
-      }
-      catch (IllegalAccessException iaExc) {
-        assert false : "IllegalAccessExceptionshould not happen: " + iaExc; //$NON-NLS-1$
-      }
-      catch (InvocationTargetException itExc) {
-        assert false : "InvocationTargetExceptionshould not happen: " + itExc; //$NON-NLS-1$
-      }
-      result = manySet.contains(manyObject);
     }
     return result;
   }
 
   /**
-   * This method gathers the type invariants for a set that implements
-   * the to-many part of a bidirectional one-to-many association.
+   * This method gathers the type invariants for a {@link Set} that implements
+   * the to-many part of a bidirectional one-to-many association. For a
+   * bidirectional associaton {@code A <-*as---b1-> B}, this method should be used
+   * as a type invariant in class {@code B}. The invariant should
+   * be {@code @invar bidr1ToN(getAs(), "b", this);}. The invariant then
+   * expresses that, for all instances {@code b} of {@code B}, for each reference {@code a}
+   * contained in the {@link Set} {@code b.getAs()}, {@code a.getB()} refers to {@code b}.
    *
-   * @param     manyCollection
-   *            The {@link Collection} that is to contain many references to instances of
+   * @param     manySet
+   *            The {@link Set} that is to contain many references to instances of
    *            type <code>manyType</code>. This set is owned by <code>oneObject</code>.
-   * @param     manyType
-   *            The type on the many side of the one-to-many association.
    * @param     toOneReferencePropertyName
    *            The name of the property that holds the to-one reference
    *            on instances of <code>manyType</code> in <code>manySet</code>.
    *            This property should refer to <code>oneObject</code>.
-   *            <code>manyType</code> must have a getter for this property
+   *            <code>_Many_</code> must have a getter for this property
    *            that returns a reference to an instance of the type of
    *            <code>oneObject</code>.
    * @param     oneObject
    *            The instance on the one-side of the one-to-many association,
    *            that owns <code>manySet</code>.
-   * @pre       manyType != null;
    * @pre       Beans.hasPropertyReadMethod(manyType, toOneReferenceProperty);
    * @return    (manySet != null)
    *            && cC:noNull(manySet)
-   *            && cC:instanceOf(manyType)
-   *            && (forall Object o; manySet.contains(o);
+   *            && (forall _Many_ o; manySet.contains(o);
    *                    o[toOneReferencePropertyName] == oneObject);
    */
-  public static final boolean bidir1ToN(final Collection manyCollection,
-                                        final Class manyType,
-                                        final String toOneReferencePropertyName,
-                                        final Object oneObject) {
-    assert manyType != null;
-    boolean result = (manyCollection != null) && (!manyCollection.contains(null));
+  public static final <_One_, _Many_> boolean bidir1ToN(final Set<_Many_> manySet,
+                                                       final String toOneReferencePropertyName,
+                                                       final _One_ oneObject) {
+    boolean result = (manySet != null) && (!manySet.contains(null));
     if (result) {
-      Iterator iter = manyCollection.iterator();
-      while (iter.hasNext()) {
-        Object manyObject = iter.next();
+      for (_Many_ manyObject : manySet) {
         try {
-          if ((!manyType.isInstance(manyObject))
-              || (Reflection.getPropertyValue(manyObject, toOneReferencePropertyName)
-                      != oneObject)) {
+          if (Reflection.getPropertyValue(manyObject, toOneReferencePropertyName) != oneObject) {
             return false; // break
           }
           // else continue
         }
-        catch (NullPointerException npExc) {
-          assert false : "NullPointerExceptionshould not happen: " + npExc; //$NON-NLS-1$
-        }
-        catch (IntrospectionException iExc) {
-          assert false : "IntrospectionExceptionshould not happen: " + iExc; //$NON-NLS-1$
-        }
-        catch (NoSuchMethodException nsmExc) {
-          assert false : "NoSuchMethodExceptionshould not happen: " + nsmExc; //$NON-NLS-1$
-        }
-        catch (IllegalArgumentException iaExc) {
-          assert false : "IllegalArgumentExceptionshould not happen: " + iaExc; //$NON-NLS-1$
-        }
-        catch (IllegalAccessException iaExc) {
-          assert false : "IllegalAccessExceptionshould not happen: " + iaExc; //$NON-NLS-1$
-        }
-        catch (InvocationTargetException itExc) {
-          assert false : "InvocationTargetExceptionshould not happen: " + itExc; //$NON-NLS-1$
+        catch (CannotGetValueException e) {
+          assert false : "CannotGetValueException should not happen: " + e;
         }
       }
-      // else result is still true
+      // if we get here, result is still true
     }
     return result;
   }
@@ -170,7 +142,14 @@ public class Associations {
   /**
    * This method gathers the postconditions for the setter on the many-side
    * of a bidirectional one-to-many association. If the old and the new
-   * <code>oneObject</code> are the same, nothing changes.
+   * <code>oneObject</code> are the same, nothing changes. For a bidirectional associaton
+   * {@code A <-*as---b1-> B}, this postcondition should be applied to a method
+   * {@code A.setB(B )} as {@code @invar bidir1ToN(new.getB(), getB(), b, "as", this},
+   * This means that, for the instance @{a} of type {@code A}, the old associated
+   * {@code B} instance {@code a.getB()} no longer is associated with {@code a},
+   * i.e., {@code a.getB().getAs()} no longer contains {@code a}, and the new
+   * associated {@code B} instance {@code b} is returned by {@code new a.getB()}
+   * and {@code a} is contained in the set {@code b.getAs()} in the new state.
    *
    * @param   reference
    *          The object the <code>manyObject</code>refers to; this is the
@@ -185,10 +164,10 @@ public class Associations {
    *          The object <code>manyObject</code> should refer to in the new state,
    *          after the mutator is called. This may be <code>null</code>.
    *          This is the actual parameter of the setter.
-   * @param   manyCollectionPropertyName
+   * @param   manySetPropertyName
    *          The name of the property that holds the to-many reference
    *          on instances of the <dfn>oneObject</dfn>.
-   *          This property should return a {@link Collection} instance,
+   *          This property should return a {@link Set} instance,
    *          that contains references to the <code>manyObject</code>s.
    *          This property of the <code>oldOneObject</code> should no
    *          longer contain a reference to <code>manyObject</code>,
@@ -198,22 +177,22 @@ public class Associations {
    *          The instance on the many-side of the one-to-many association,
    *          that holds a <code>reference</code> to the <dfn>oneObject</code>.
    * @pre     manyObject != null;
-   * @pre     Beans.hasPropertyReadMethod(oldOneReference, manyCollectionPropertyName);
-   * @pre     Beans.hasPropertyReadMethod(newOneReference, manyCollectionPropertyName);
+   * @pre     Beans.hasPropertyReadMethod(oldOneReference, manySetPropertyName);
+   * @pre     Beans.hasPropertyReadMethod(newOneReference, manySetPropertyName);
    * @result  (newOneObject != oldOneObject)
    *              ? ((reference == newOneObject)
    *                    && ((oldOneObject != null)
-   *                        ? (! oldOneObject[manyCollectionPropertyName]
+   *                        ? (! oldOneObject[manySetPropertyName]
    *                                    .contains(manyObject)))
    *                    && ((newOneObject != null)
-   *                        ? (newOneObject[manyCollectionPropertyName]
+   *                        ? (newOneObject[manySetPropertyName]
    *                                    .contains(manyObject))));
    */
-  public static final boolean bidir1ToN(final Object reference,
-                                        final Object oldOneObject,
-                                        final Object newOneObject,
-                                        final String manyCollectionPropertyName,
-                                        final Object manyObject) {
+  public static final <_One_, _Many_> boolean bidir1ToN(final _One_ reference,
+                                                        final _One_ oldOneObject,
+                                                        final _One_ newOneObject,
+                                                        final String manySetPropertyName,
+                                                        final _Many_ manyObject) {
     assert manyObject != null;
     if (newOneObject == oldOneObject) {
       return true;
@@ -221,46 +200,28 @@ public class Associations {
     boolean result = (reference == newOneObject);
     if (result && (oldOneObject != null)) {
       try {
-        Collection oldManyCollection
-            = (Collection)Reflection.getPropertyValue(oldOneObject, manyCollectionPropertyName);
-        result &= (! oldManyCollection.contains(manyObject));
+        @SuppressWarnings("unchecked") Set<_Many_> oldManySet
+            = (Set<_Many_>)Reflection.getPropertyValue(oldOneObject, manySetPropertyName);
+        result &= (! oldManySet.contains(manyObject));
       }
       catch (NullPointerException e) {
         assert false : "NullPointerExceptionshould not happen: " + e;
       }
-      catch (IntrospectionException e) {
-        assert false : "IntrospectionExceptionshould not happen: " + e;
-      }
-      catch (NoSuchMethodException e) {
-        assert false : "NoSuchMethodExceptionshould not happen: " + e;
-      }
-      catch (IllegalAccessException e) {
-        assert false : "IllegalAccessExceptionshould not happen: " + e;
-      }
-      catch (InvocationTargetException e) {
-        assert false : "InvocationTargetExceptionshould not happen: " + e;
+      catch (CannotGetValueException e) {
+        assert false : "CannotGetValueException should not happen: " + e;
       }
     }
     if (result && (newOneObject != null)) {
       try {
-        Collection newManyCollection
-            = (Collection)Reflection.getPropertyValue(newOneObject, manyCollectionPropertyName);
-        result &= (newManyCollection.contains(manyObject));
+        @SuppressWarnings("unchecked") Set<_Many_> newManySet
+            = (Set<_Many_>)Reflection.getPropertyValue(newOneObject, manySetPropertyName);
+        result &= (newManySet.contains(manyObject));
       }
       catch (NullPointerException e) {
-        assert false : "NullPointerExceptionshould not happen: " + e;
+        assert false : "NullPointerException should not happen: " + e;
       }
-      catch (IntrospectionException e) {
-        assert false : "IntrospectionExceptionshould not happen: " + e;
-      }
-      catch (NoSuchMethodException e) {
-        assert false : "NoSuchMethodExceptionshould not happen: " + e;
-      }
-      catch (IllegalAccessException e) {
-        assert false : "IllegalAccessExceptionshould not happen: " + e;
-      }
-      catch (InvocationTargetException e) {
-        assert false : "InvocationTargetExceptionshould not happen: " + e;
+      catch (CannotGetValueException e) {
+        assert false : "CannotGetValueException should not happen: " + e;
       }
     }
     return result;
